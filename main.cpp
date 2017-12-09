a8637f307a24bf83a5800cc7332f9b875b2330f2
Jaymel Dash <jwdash@email.sc.edu>
2017-12-08 19:54:33 -0500

    Updated Header

--- main.cpp
+++ main.cpp
@@ -1,27 +1,27 @@
 /***************************************************************************
- * Name: Michael Afrin
- * Email: mafrin@email.sc.edu
- * Date: 12/4/2017
+ * Name: Michael Afrin & Jaymel Dash                                                                                     *
+ * Email: mafrin@email.sc.edu | jwdash@email.sc.edu                                                             *
+ * Date: 12/4/2017                                                                                                                       *
  * Function:
  * Input:
  * Output:
  * Additional Comments:
- *                                                                         *
- * Copyright (C) 2017 by Michael Afrin                                     *
+ *                                                                                                                                                  *
+ * Copyright (C) 2017 by Michael Afrin & Jaymel Dash                                                           *
  ***************************************************************************/
 
 #include <iostream>
+#include <stdlib.h>
 #include <ctime>
 
-#include "World/World.h"
+#include "World/World.cpp"
 
 int main() {
     // Sets the c++ random seed to the current time. Ensures random generates different numbers for each execution
     //srand(time(NULL));
 
-
     World world;
-    world.init();
+    // world.init();
 
     int day = 1;
     while(true) {
